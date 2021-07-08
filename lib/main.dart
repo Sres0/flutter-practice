cc22f793627621e9d7c2f317acc3d1cff62276e6
Sres0 <susanarepo@gmail.com>
2021-07-07 21:32:05 -0500

    End page

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,21 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  //static const works as well
+  final qAndAs = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Blue', 'Red', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Horse', 'Squid']
+    },
+    {
+      'questionText': 'What\'s your favorite card?',
+      'answers': ['Ace of Spades', 'King of Hearts']
+    },
+  ];
 
   void _answerQuestion() {
     setState(
@@ -27,34 +42,25 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext ctx) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Blue', 'Red', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Horse', 'Squid']
-      },
-      {
-        'questionText': 'What\'s your favorite card?',
-        'answers': ['Ace of Spades', 'King of Hearts']
-      },
-    ];
+    //final instead of var because it's not gonna change, but const instead of final because it remains the same since the app starts--we know what the final value will be
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Personality test')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Question(questions[_questionIndex]['questionText'] as String),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < qAndAs.length
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Question(qAndAs[_questionIndex]['questionText'] as String),
+                  ...(qAndAs[_questionIndex]['answers'] as List<
+                          String>) //... spread operator makes every value of the list an element
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(child: Text('End c:')),
       ),
     );
   }
