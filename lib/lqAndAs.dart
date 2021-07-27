813e8c3a39b24f0fe9b32a171735df0caa6966a9
Sres0 <susanarepo@gmail.com>
2021-07-27 15:30:58 -0500

    added scores

--- lib/lqAndAs.dart
+++ lib/lqAndAs.dart
@@ -1,14 +1,28 @@
 final qAndAs = const [
   {
     'questionText': 'What\'s your favorite color?',
-    'answers': ['Blue', 'Red', 'White']
+    'answers': [
+      {'text': 'Black or white', 'score': 3},
+      {'text': 'Blue or Red', 'score': 0},
+      {'text': 'Green or Orange', 'score': 2},
+      {'text': 'Other', 'score': 1},
+    ]
   },
   {
     'questionText': 'What\'s your favorite animal?',
-    'answers': ['Dog', 'Cat', 'Horse', 'Squid']
+    'answers': [
+      {'text': 'Cat or Dog', 'score': 0},
+      {'text': 'Horse or Wolf', 'score': 1},
+      {'text': 'Elephant or Giraffe', 'score': 2},
+      {'text': 'Penguin or Snake', 'score': 3},
+    ]
   },
   {
     'questionText': 'What\'s your favorite card?',
-    'answers': ['Ace of Spades', 'King of Hearts']
+    'answers': [
+      {'text': 'Queen of Hearts', 'score': 0},
+      {'text': 'Joker', 'score': 2},
+      {'text': 'Ace of Spades', 'score': 1},
+    ]
   },
 ];
