2d085db3f9f8c62a15d888c38e1b265152da2851
Sres0 <susanarepo@gmail.com>
2021-07-27 16:23:14 -0500

    result score and personalized text

--- lib/result.dart
+++ lib/result.dart
@@ -5,10 +5,32 @@ class Result extends StatelessWidget {
 
   Result(this.finalScore);
 
+  String get finalText {
+    //getter doesn't receive arguments
+    String finalText;
+    if (finalScore > 5) {
+      finalText = 'You\'re awesome!';
+    } else if (finalScore > 3) {
+      finalText = 'You\'re... cool';
+    } else if (finalScore > 1) {
+      finalText = 'You\'re okay';
+    } else {
+      finalText = 'Not gonna lie. You\'re BoRiNg.';
+    }
+    return finalText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Final score: ' + finalScore.toString()),
+      child: Text(
+        'Final score: ' + finalScore.toString() + '\n\n' + finalText,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 35,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
