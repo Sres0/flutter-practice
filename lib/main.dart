d5fe330d17b89646f9cb6a62931c7248ede1b7b1
Sres0 <susanarepo@gmail.com>
2021-07-27 19:47:24 -0500

    after solution

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,10 @@
 import 'package:flutter/material.dart';
 
 import './textControl.dart';
-import './texts.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _index = 0;
-
-  change() {
-    if (_index < texts.length - 1) {
-      setState(() => _index += 1);
-    } else if (_index > 0) {
-      setState(() => _index = 0);
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
@@ -32,20 +16,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('ProjectA'),
         ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextControl(_index),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: change,
-              child: Text('Change!'),
-            ),
-          ],
-        )),
+        body: Center(child: TextControl()),
       ),
     );
   }
--- lib/textControl.dart
+++ lib/textControl.dart
@@ -2,19 +2,46 @@ import 'package:flutter/material.dart';
 
 import './texts.dart';
 
-class TextControl extends StatelessWidget {
-  int index;
+class TextControl extends StatefulWidget {
+  TextControl();
+  @override
+  _TextControlState createState() => _TextControlState();
+}
 
-  TextControl(this.index);
+class _TextControlState extends State<TextControl> {
+  int _index = 0;
+
+  void change() {
+    if (_index < texts.length - 1) {
+      setState(() => _index += 1);
+    } else if (_index > 0) {
+      setState(() => _index = 0);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      texts[index],
-      style: TextStyle(
-        fontSize: 20,
-      ),
-      textAlign: TextAlign.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            texts[_index],
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: change,
+          child: Text('Change!'),
+        ),
+      ],
     );
   }
 }
