1483e5daed63655667cd04edaf4f6c347de4f826
Sres0 <susanarepo@gmail.com>
2021-07-29 10:23:44 -0500

    styling tx card

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -11,33 +11,54 @@ class TransactionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
       color: Colors.grey.shade700,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                shape: BoxShape.rectangle,
+                border: Border.all(
+                  color: Colors.white70,
+                  width: 2,
+                ),
+              ),
+              child: Text(
+                '\$${tx.amount}', //Instead of toString
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white70,
+                ),
               ),
             ),
-            child: Text(
-              tx.amount.toString(),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              Text(tx.title),
-              Text(tx.date.toString()),
-            ],
-          )
-        ],
+            Column(
+              children: <Widget>[
+                Text(
+                  tx.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white70,
+                      fontSize: 17),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  tx.date.toString(),
+                  style: TextStyle(color: Colors.white54),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
