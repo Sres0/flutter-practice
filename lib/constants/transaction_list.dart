1dce48abd4369cfd9a8a95294d7dc9bdf6777c60
Sres0 <susanarepo@gmail.com>
2021-07-29 12:01:10 -0500

    changed prices for double decimals

--- lib/constants/transaction_list.dart
+++ lib/constants/transaction_list.dart
@@ -4,13 +4,13 @@ import '../widgets/transaction.dart';
 
 List<Transaction> transactions = [
   Transaction(
-    amount: 0.00,
+    amount: 0.99,
     date: DateTime.now(),
     id: '0',
     title: 'No one ever',
   ),
   Transaction(
-    amount: 10.00,
+    amount: 10.55,
     date: DateTime.now(),
     id: '1',
     title: 'Easy peasy',
