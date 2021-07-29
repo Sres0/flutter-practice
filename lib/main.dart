70d9bb07d428f8000cbb6867d6abaca19d4cdab7
Sres0 <susanarepo@gmail.com>
2021-07-29 11:58:04 -0500

    added new tx fields

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,52 @@ class MyHomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           ContainerCard('Weekly Balance'),
-          // Column(children: <Widget>[
-          //   ...transactions.map((tx) {
-          //     return Card(child: Text(tx.title));
-          //   }).toList(),
-          // ]), //spread operator
+          CardForm(),
           Column(
             children: transactions.map((tx) {
               return TransactionCard(tx);
             }).toList(),
           )
         ],
+      ),
+    );
+  }
+}
+
+class CardForm extends StatelessWidget {
+  final VoidCallback onPressed;
+  const CardForm(this.onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        color: Colors.grey.shade600,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.white70)),
+              onPressed: onPressed,
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(color: Colors.white70),
+              ),
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
