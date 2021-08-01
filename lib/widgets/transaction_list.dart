490711c0007fe7990553cc8a20cc50c6354f5c76
Sres0 <susanarepo@gmail.com>
2021-07-31 23:59:44 -0500

    tx list from card to ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/transaction.dart';
-import './transaction_card.dart';
+// import './transaction_card.dart';
 
 List<Transaction> transactions = [
   Transaction(
@@ -49,7 +49,33 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return TransactionCard(transactions[index]);
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ListTile(
+                    tileColor: Theme.of(context).primaryColor,
+                    leading: Padding(
+                      padding: EdgeInsets.all(2),
+                      child: CircleAvatar(
+                        radius: 30,
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      transactions[index].date.toString(),
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                    trailing: null,
+                  ),
+                );
+                // return TransactionCard(transactions[index]);
               },
               itemCount: transactions.length,
             ),
