29a9cee399a5374e951ebb3a7120ac4dc0f3228f
Sres0 <susanarepo@gmail.com>
2021-08-10 00:10:26 -0400

    +book details screen

---
+++ lib/screens/book_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsScreen extends StatelessWidget {
+  static const routeName = '/details';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Book!'));
+  }
+}
